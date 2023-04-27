6afff3b92f5ce51f59a54269a055aef8dc6eeae2
Goosius1 <goosius1@gmail.com>
2023-04-27 13:19:02 +0100

    More work on bat roles

--- src/main/java/io/github/townyadvanced/townycombat/utils/TownyCombatBattlefieldRoleUtil.java
+++ src/main/java/io/github/townyadvanced/townycombat/utils/TownyCombatBattlefieldRoleUtil.java
@@ -3,11 +3,10 @@ package io.github.townyadvanced.townycombat.utils;
 import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Translatable;
-import com.palmergames.bukkit.towny.object.Translator;
 import io.github.townyadvanced.townycombat.events.BattlefieldRole;
 import io.github.townyadvanced.townycombat.metadata.TownyCombatResidentMetaDataController;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,14 +15,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.UUID;
 
 public class TownyCombatBattlefieldRoleUtil {
 
     public static final List<Material> lightPlayerArmour = Arrays.asList(Material.LEATHER_BOOTS, Material.LEATHER_CHESTPLATE, Material.LEATHER_HELMET, Material.LEATHER_LEGGINGS);
 
     public static final Map<Material, BattlefieldRole> materialBattlefieldRoleMap;
-    
+
     static {
         //Create the material-battlefieldRole map
         materialBattlefieldRoleMap = new HashMap<>();
@@ -33,9 +31,13 @@ public class TownyCombatBattlefieldRoleUtil {
     }
 
     public static BattlefieldRole getBattlefieldRole(Material material) {
-        return materialBattlefieldRoleMap.get(material);
+        BattlefieldRole result = materialBattlefieldRoleMap.get(material);
+        if(result == null)
+            return BattlefieldRole.HEAVY;
+        else 
+            return result;
     }
-    
+
     public static BattlefieldRole getBattlefieldRole(Resident resident) {
         String roleAsString = TownyCombatResidentMetaDataController.getBattlefieldRole(resident);
         return BattlefieldRole.parseString(roleAsString);
@@ -47,24 +49,19 @@ public class TownyCombatBattlefieldRoleUtil {
         Resident resident = TownyAPI.getInstance().getResident(inventoryClickEvent.getWhoClicked().getUniqueId());
         if(resident == null)
             return;
-        BattlefieldRole materialBattlefieldRole = getBattlefieldRole(inventoryClickEvent.getCursor().getType());
+        ItemStack item = inventoryClickEvent.getCursor();
+        BattlefieldRole materialBattlefieldRole = getBattlefieldRole(item.getType());
         BattlefieldRole playerBattlefieldRole = getBattlefieldRole(resident);
         
         if(materialBattlefieldRole != playerBattlefieldRole) {
             //Compose error message
             String requiredRoleText = Translatable.of(materialBattlefieldRole.getNameKey()).translate(Locale.ROOT); 
-            String currentRoleText = Translatable.of(playerBattlefieldRole.getNameKey()).translate(Locale.ROOT); ;
             Translatable errorMessage = Translatable.of("msg_warning_cannot_wear_this_armour", requiredRoleText);
-            if(canPlayerChangeRoleNow(resident)) {
-                errorMessage.append(Translatable.of("msg_warning_do_you_wish_to_change_role_now", currentRoleText, requiredRoleText));
-            } else {
-                errorMessage.append(Translatable.of("msg_warning_cannot_change_role_now", getFormattedTimeUntilNextRoleChange(resident)));
-            }
-            //Send error message
+            errorMessage.append(Translatable.of("msg_warning_how_to_view_and_change_role"));
+            //Send warning message
             Messaging.sendErrorMsg(inventoryClickEvent.getWhoClicked(), errorMessage);
-            //Cancel event
-            inventoryClickEvent.setCancelled(true);
-            //TODO - Ensure item does not disappear
+            //Drop the item (Note: cancelling the event would delete the item)
+            inventoryClickEvent.getWhoClicked().getWorld().dropItem(inventoryClickEvent.getWhoClicked().getLocation(), item);
         }
     }
     
@@ -77,7 +74,7 @@ public class TownyCombatBattlefieldRoleUtil {
         return "Never hur hur";
     }
 
-    public static void processPlayerWeaponWieldAttempt(InventoryClickEvent event) {
+    public static void processPlayerWeaponWieldAttempt(InventoryClickEvent inventoryClickEvent) {
         
     }
 }
