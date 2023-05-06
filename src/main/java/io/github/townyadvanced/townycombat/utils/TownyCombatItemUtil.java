c972430ce579042c147281b59e1b3027619b0835
Goosius1 <50219223+Goosius1@users.noreply.github.com>
2023-05-06 20:29:10 +0100

    Remove warhammer (#88)
    * Integrated horses with battlefield roles
    
    * Removed encumbrance & speed effects
    
    * Removed auto-potting .... this will soon be replaced
    
    * Remove warhammer
    
    ---------
    
    Co-authored-by: Goosius1 <goosius1@gmail.com>

--- src/main/java/io/github/townyadvanced/townycombat/utils/TownyCombatItemUtil.java
+++ src/main/java/io/github/townyadvanced/townycombat/utils/TownyCombatItemUtil.java
@@ -2,8 +2,8 @@ package io.github.townyadvanced.townycombat.utils;
 
 import io.github.townyadvanced.townycombat.settings.TownyCombatSettings;
 import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
@@ -20,15 +20,8 @@ public class TownyCombatItemUtil {
     public static final Material[] NATIVE_SPEAR_MATERIALS = new Material[]{null, null, Material.IRON_INGOT, null, Material.STICK, null, Material.STICK, null, null}; 			
     public static final int NATIVE_SPEAR_SHARPNESS_LEVEL = 8;
 
-    public static final Material NATIVE_WARHAMMER_PLACEHOLDER_MATERIAL = Material.WOODEN_AXE;
-    public static final Material[] NATIVE_WARHAMMER_MATERIALS = new Material[]{null, null, Material.STONE, null, Material.STICK, null, Material.STICK, null, null}; 			
-    public static final int NATIVE_WARHAMMER_SHARPNESS_LEVEL = 10;
-
     //After we have identified a weapon as spear or not spear, we list it here
     public static Map<ItemStack, Boolean> spearIdentificationMap = new HashMap<>();
-
-    //After we have identified a weapon as warhammer or not warhammer, we list it here
-    public static Map<ItemStack, Boolean> warhammerIdentificationMap = new HashMap<>();
 
     /**
      * Check if the given item is a vanilla placeholder version of a custom item.
@@ -42,11 +35,6 @@ public class TownyCombatItemUtil {
                 && item.getType() == NATIVE_SPEAR_PLACEHOLDER_MATERIAL
                 && !isSpear(item)) {
             return true;  //Vanilla wooden sword
-        } else if (TownyCombatSettings.isNewItemsWarhammerEnabled()
-                && TownyCombatSettings.isNewItemsWarhammerNativeWeaponEnabled()
-                && item.getType() == NATIVE_WARHAMMER_PLACEHOLDER_MATERIAL
-                && !isWarhammer(item)) {
-            return true;  //Vanilla stone axe
         } else {
             return false;
         }
@@ -63,12 +51,6 @@ public class TownyCombatItemUtil {
                 && TownyCombatSettings.isNewItemsSpearNativeWeaponEnabled()
                 && material == NATIVE_SPEAR_PLACEHOLDER_MATERIAL) {
             return true;
-
-        } else if (TownyCombatSettings.isNewItemsWarhammerEnabled()
-                && TownyCombatSettings.isNewItemsWarhammerNativeWeaponEnabled()
-                && material == NATIVE_WARHAMMER_PLACEHOLDER_MATERIAL) {
-            return true;
-
         } else {
             return false;
         }
@@ -95,23 +77,6 @@ public class TownyCombatItemUtil {
 			itemMeta.setLore(lore);
 			result.setItemMeta(itemMeta);
 			return result;
-        
-        } else if(TownyCombatSettings.isNewItemsWarhammerEnabled()
-                && TownyCombatSettings.isNewItemsWarhammerNativeWeaponEnabled()
-                && doesMatrixMatch(event.getInventory().getMatrix(), NATIVE_WARHAMMER_MATERIALS)) {
-			ItemStack result = new ItemStack(NATIVE_WARHAMMER_PLACEHOLDER_MATERIAL);
-			ItemMeta itemMeta = result.getItemMeta();
-			itemMeta.setDisplayName(TownyCombatSettings.getNewItemsWarhammerNativeWeaponName());
-			//Add enchants
-			itemMeta.addEnchant(Enchantment.DAMAGE_ALL, NATIVE_WARHAMMER_SHARPNESS_LEVEL, true);
-			itemMeta.addEnchant(Enchantment.KNOCKBACK, 1, true);
-			//Add lore
-			List<String> lore = new ArrayList<>();
-			lore.add(TownyCombatSettings.getNewItemsWarhammerLore());
-			itemMeta.setLore(lore);
-			result.setItemMeta(itemMeta);
-			return result;
-
         } else {
             return event.getInventory().getResult();
         }
@@ -144,27 +109,6 @@ public class TownyCombatItemUtil {
         }
        return true;
     }
-
-    /**
-     * Roll the dice. If the number comes us, break the item
-     *
-     * @param itemHolder the holder of the item
-     * @param offHand Is the item in offhand?, otherwise it is main hand.
-     * @param percentageChange the chance
-     */
-    public static void rollBreakItemInHand(Player itemHolder, boolean offHand, double percentageChange) {
-        double normalizedChance = percentageChange / 100;
-        double num = Math.random();
-        if(num < normalizedChance) {
-            if(offHand) {
-                itemHolder.getInventory().setItemInOffHand(null);
-            } else {
-                itemHolder.getInventory().setItemInMainHand(null);
-            }
-            if(itemHolder.getLocation().getWorld() != null)
-                itemHolder.getLocation().getWorld().playSound(itemHolder.getLocation(), Sound.ITEM_SHIELD_BREAK ,1f, 0.5f);
-        }        
-    }
  
     /**
      * Determine is a given item is a spear
@@ -194,38 +138,6 @@ public class TownyCombatItemUtil {
                 }
             }
             spearIdentificationMap.put(item, result);
-        }
-        return result;
-    }
-
-    /**
-     * Determine is a given item is a warhammer
-     * @param item the item
-     *
-     * @return true if the item is a warhammer
-     */
-    public static boolean isWarhammer(ItemStack item) {
-        Boolean result = warhammerIdentificationMap.get(item);
-        if(result == null) {
-            result = false;
-            if(item.getItemMeta() != null) {
-                final int CONFIGURED_CUSTOM_MODEL_DATA_ID = TownyCombatSettings.getNewItemsWarhammerCustomModelDataID();
-                if(CONFIGURED_CUSTOM_MODEL_DATA_ID != -1) { 
-                    if(item.getItemMeta().hasCustomModelData()
-                            && item.getItemMeta().getCustomModelData() == CONFIGURED_CUSTOM_MODEL_DATA_ID) {
-                        result = true;     
-                    }
-                
-                } else if (item.getItemMeta().getLore() != null) {
-                    for(String loreLine: item.getItemMeta().getLore()) {
-                        if(loreLine.equals(TownyCombatSettings.getNewItemsWarhammerLore())) {
-                            result = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            warhammerIdentificationMap.put(item, result);
         }
         return result;
     }
